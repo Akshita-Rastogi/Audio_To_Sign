bcd0487791153a1121d6f944b1d9b71241595f60
Akshita-Rastogi <65523112+Akshita-Rastogi@users.noreply.github.com>
2021-12-28 23:53:03 +0530

    Update settings.py

--- A2SL/settings.py
+++ A2SL/settings.py
@@ -16,7 +16,7 @@ from django.conf import settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+FILE_CHARSET = 'utf-16'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
